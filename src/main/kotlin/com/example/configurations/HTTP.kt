0788fefcdaf3cfe2cd53d3fd658adf3f07edaf6c
MrKekovich <mrkekovich.official@gmail.com>
2024-01-15 13:43:59 +0300

    refactor: more allowed methods and headers for CORS

--- src/main/kotlin/com/example/configurations/HTTP.kt
+++ src/main/kotlin/com/example/configurations/HTTP.kt
@@ -8,12 +8,14 @@ import io.ktor.server.routing.*
 
 fun Application.configureHTTP() {
     install(CORS) {
-        allowMethod(HttpMethod.Options)
+        allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Put)
+        allowMethod(HttpMethod.Post)
+        allowMethod(HttpMethod.Head)
         allowMethod(HttpMethod.Patch)
         allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Post)
-        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Options)
+        allowHeader(HttpHeaders.ContentType)
         allowHeader(HttpHeaders.Authorization)
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
